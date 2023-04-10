2623a8ed7c428390ac7423038cc03664c745a72b
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-04-10 22:03:38 +0300

    add settings

--- src/settings.h
+++ src/settings.h
@@ -1,11 +1,15 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-const char * BACK_FILENAME = "../data/back.bmp" ;
-const char *FRONT_FILENAME = "../data/front.bmp";
-const char *BLEND_FILENAME = "../data/blend.bmp";
+const char * BACK_FILENAME = "../data/back.bmp" ;   // background filename
+const char *FRONT_FILENAME = "../data/front.bmp";   // foreground filename
+const char *BLEND_FILENAME = "../data/blend.bmp";   // blending   filename
 
-const int FRONT_OFFSET_X    = 400;
-const int FRONT_OFFSET_Y    = 400;
+const int FRONT_OFFSET_X   = 400;                   // horizontal offset of the foreground
+const int FRONT_OFFSET_Y   = 400;                   //   vertical offset of the foreground
+
+const int TEST_HEIGHT      = 1000;                  // height (in pixels) of the image to test
+const int TEST_WIDTH       = 1000;                  // width  (in pixels) of the image to test
+const int TEST_NUM         = 1000;                  // number of tests to average the result
 
 #endif //SETTINGS_H
